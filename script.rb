70daa5201ee7b4a6dffd5b62c5ae159dd6ddc69b
kswope <github-kevdev@snkmail.com>
2015-02-11 17:07:29 -0500

    .

--- script.rb
+++ script.rb
@@ -1,20 +1,6 @@
 require 'pp'
 
-class MyInt
 
-  def initialize(int)
-    @int = int
-  end
-
-  def +(other)
-    @int + other
-  end
-
-end
-
-int = MyInt.new(1)
-
-p int + 1 #=> 2
-
-int += 1
-p int     #=> 2
+var = 1
+var ||= 2
+p var

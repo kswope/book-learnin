4e7de5abd205b6c0d6afb18c0e5f080f80a2584f
kswope <github-kevdev@snkmail.com>
2015-02-11 17:26:16 -0500

    .

--- script.rb
+++ script.rb
@@ -1,14 +1,16 @@
 require 'pp'
 
 
-# same as above
-var = 1
-var &&= 2 # same as next line
-# var = var && 2 
-p var #=> 2
 
-var = nil
-var &&= 2 # same as next line
-# var = var && 2
-p var #=> nil
+class MyClass
+  def +()
+    :+
+  end
 
+  def -()
+    :-
+  end
+end
+
+p MyClass.new +
+p MyClass.new -

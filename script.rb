b1f4e71407f9059a2ea08039e7d4c0d482c6e8ea
kswope <github-kevdev@snkmail.com>
2015-02-12 23:28:40 -0500

    .

--- script.rb
+++ script.rb
@@ -8,7 +8,7 @@ p "my string"[3..-1] #=> "string" use a range if you have two positions
 p "my string"['string'] #=> 'string' # finds and returns a substring
 p "my string"['no string'] #=> nil
 
-target = 'str'
+target = /str/
 if 'my string'[target]
   puts "found target #{target}"
 end

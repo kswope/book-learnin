dfd06ad50cceef47450aed1edb11d4e81e6f0ab9
kswope <github-kevdev@snkmail.com>
2015-02-16 03:15:27 -0500

    .

--- script.rb
+++ script.rb
@@ -2,7 +2,14 @@ require 'pp'
 require 'ap'
 
 
-e = [1,2,3,4,5,6]
+class MyClass
 
-p e.inject([]){|acc, x| acc << x; acc} 
-p e.reduce([]){|acc, x| acc << x; acc} 
+  def initialize(x)
+    @var = x
+  end
+
+  def <=>(other)
+    self <=> other
+  end
+
+end

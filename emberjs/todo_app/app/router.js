50697e68f3c32a877724cccabba3a225c74f7bbc
kswope <github-kevdev@snkmail.com>
2015-03-22 15:31:48 -0400

    .

--- emberjs/todo_app/app/router.js
+++ emberjs/todo_app/app/router.js
@@ -6,8 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('about', {path: '/about'});
-  this.resource('favs', {path: '/favs'});
+  this.route('about');
+  this.route('favs');
 });
 
 export default Router;
--- emberjs/todo_app/app/routes/about.js
+++ emberjs/todo_app/app/routes/about.js
@@ -1,8 +1,23 @@
 import Ember from 'ember';
 
+
+
+// export default Ember.Route.extend({
+//   setupController: function( controller ) {
+//     controller.set( 'title', 'ToDo/About' );
+//   },
+// });
+
+
+
 export default Ember.Route.extend( {
-  setupController: function( controller ) {
-    controller.set( 'title', 'ToDo/About' );
+  model: function() {
+    return [ {
+      title: "Tomster",
+      url: "http://emberjs.com/images/about/ember-productivity-sm.png"
+      }, {
+      title: "Eiffel Tower",
+      url: "http://emberjs.com/images/about/ember-structure-sm.png"
+      } ];
   }
 } );
-

2560aeb3be65885c64e79fcde91129ef31245b3d
kswope <github-kevdev@snkmail.com>
2015-03-27 16:49:39 -0400

    .

---
+++ emberjs/crm.saved/app/adapters/store.js
@@ -0,0 +1,10 @@
+import ApplicationAdapter from './application';
+
+// export default ApplicationAdapter.extend({
+// });
+
+export default ActiveModelAdapter.extend({
+});
+
+
+
---
+++ emberjs/crm.saved/app/models/lead.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export
+default DS.Model.extend( {
+
+  firstName: DS.attr( 'string' ),
+  lastName:  DS.attr( 'string' ),
+  email:     DS.attr( 'string' ),
+  phone:     DS.attr( 'string' ),
+  notes:     DS.attr( 'string' ),
+  status:    DS.attr( 'string', { defaultValue: 'new' } ),
+
+} );
